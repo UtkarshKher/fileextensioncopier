843b6b2d537b46ebb8a7c2367be771700ad87a75
UtkarshKher <38111424+UtkarshKher@users.noreply.github.com>
2020-01-26 22:51:23 +0530

    Update copy.py

--- copy.py
+++ copy.py
@@ -4,11 +4,11 @@ sourcedirectory = sys.argv[1]
 destinationdirectory = sys.argv[2]
 extension = sys.argv[3]
 
-print "Usage : copy.py sourcedirname destinationdirname extension"
+print "Copying file with extension", extension, "from", sourcedirectory, "to",destinationdirectory 
 
 # For usage of os.walk refer https://www.geeksforgeeks.org/os-walk-python/ 
 
 for root, dirs, files in os.walk(sourcedirectory):
-    for file_ in files:
-        if file_.endswith(extension):
-            shutil.copy(os.path.join(root, file_), os.path.join(dstDir, file_))
+    for file in files:
+        if file.endswith(extension):
+            shutil.copy(os.path.join(root, file), os.path.join(destinationdirectory, file))
